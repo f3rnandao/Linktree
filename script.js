84427585820aa5cf65633f58740eaecfa9e99fb4
f3rnandao <fernarei@gmail.com>
2023-10-03 23:34:49 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')){
+  //   html.classList.remove('light')
+  // }else{
+  //   html.classList.add('light')
+  // }
+
+  html.classList.toggle("light")
+
+  //pegar a tag imagem
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos escuros, uma jaqueta preta, barba e fundo degradee do rosa para o azul")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo")
+  }
+}
